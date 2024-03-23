89aaf59c46d7243e65ad38469794fcfdaf440b28
Vinicius Monteiro <105878542+Vinicius-IndexCode@users.noreply.github.com>
2024-03-23 13:41:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,268 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if( menuMobile.classList.contains('open')){
+        menuMobile.classList.remove('open')
+        document.querySelector('.icon').src = "menu_white_36dp.svg";
+    } else{
+        menuMobile.classList.add('open')
+        document.querySelector('.icon').src = "close_white_36dp.svg";
+    }
+}
+function clickModal(img){
+    const janelaModal = document.getElementById('janelamodal1')
+    const txtmodal = document.getElementById('txtmodal1')
+    const btnfechar = document.getElementById('btnfechar')
+
+    janelaModal.classList.remove("escondejanelamodal1");
+    janelaModal.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+
+    btnfechar.onclick=function(){
+        janelaModal.classList.add("escondejanelamodal1");
+        janelaModal.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+    btnfechar2.onclick=function(){
+        janelaModal.classList.add("escondejanelamodal1");
+        janelaModal.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+}
+function clickModal3(img){
+    const janelaModal3 = document.getElementById('janelamodal3')
+    const txtmodal = document.getElementById('txtmodal')
+    const btnfechar3 = document.getElementById('btnfechar3')
+
+    janelaModal3.classList.remove("escondejanelamodal1");
+    janelaModal3.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+
+    btnfechar3.onclick=function(){
+        janelaModal3.classList.add("escondejanelamodal1");
+        janelaModal3.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+    btnfechar20.onclick=function(){
+        janelaModal3.classList.add("escondejanelamodal1");
+        janelaModal3.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+}
+function clickModal5(img){
+    const janelaModal5 = document.getElementById('janelamodal5')
+    const txtmodal = document.getElementById('txtmodal')
+    const btnfechar5 = document.getElementById('btnfechar5')
+
+    janelaModal5.classList.remove("escondejanelamodal1");
+    janelaModal5.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+   
+    btnfechar5.onclick=function(){
+        janelaModal5.classList.add("escondejanelamodal1");
+        janelaModal5.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+   
+}
+function clickModal6(img){
+    const janelaModal6 = document.getElementById('janelamodal6')
+    const txtmodal = document.getElementById('txtmodal')
+    const btnfechar6 = document.getElementById('btnfechar6')
+
+    janelaModal6.classList.remove("escondejanelamodal1");
+    janelaModal6.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+   
+    btnfechar6.onclick=function(){
+        janelaModal6.classList.add("escondejanelamodal1");
+        janelaModal6.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+   
+}
+function clickModal8(img){
+    const janelaModal8 = document.getElementById('janelamodal8')
+    const txtmodal = document.getElementById('txtmodal')
+    const btnfechar8 = document.getElementById('btnfechar8')
+
+    janelaModal8.classList.remove("escondejanelamodal1");
+    janelaModal8.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+   
+    btnfechar8.onclick=function(){
+        janelaModal8.classList.add("escondejanelamodal1");
+        janelaModal8.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+   
+}
+function clickModal10(img){
+    const janelaModal10 = document.getElementById('janelamodal10')
+    const txtmodal = document.getElementById('txtmodal')
+    const btnfechar10 = document.getElementById('btnfechar10')
+
+    janelaModal10.classList.remove("escondejanelamodal1");
+    janelaModal10.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+   
+    btnfechar10.onclick=function(){
+        janelaModal10.classList.add("escondejanelamodal1");
+        janelaModal10.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+   
+}
+function clickModal11(img){
+    const janelaModal11 = document.getElementById('janelamodal11')
+    const txtmodal = document.getElementById('txtmodal')
+    const btnfechar11 = document.getElementById('btnfechar11')
+
+    janelaModal11.classList.remove("escondejanelamodal1");
+    janelaModal11.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+
+    btnfechar11.onclick=function(){
+        janelaModal11.classList.add("escondejanelamodal1");
+        janelaModal11.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+   
+}
+function clickModal12(img){
+    const janelaModal12 = document.getElementById('janelamodal12')
+    const txtmodal = document.getElementById('txtmodal')
+    const btnfechar12 = document.getElementById('btnfechar12')
+
+    janelaModal12.classList.remove("escondejanelamodal1");
+    janelaModal12.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+   
+    btnfechar12.onclick=function(){
+        janelaModal12.classList.add("escondejanelamodal1");
+        janelaModal12.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+   
+}
+function clickModal13(img){
+    const janelaModal13 = document.getElementById('janelamodal13')
+    const txtmodal = document.getElementById('txtmodal')
+    const btnfechar13 = document.getElementById('btnfechar13')
+
+    janelaModal13.classList.remove("escondejanelamodal1");
+    janelaModal13.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+
+    btnfechar13.onclick=function(){
+        janelaModal13.classList.add("escondejanelamodal1");
+        janelaModal13.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+}
+function clickModal14(img){
+    const janelaModal14 = document.getElementById('janelamodal14')
+    const txtmodal = document.getElementById('txtmodal')
+    const btnfechar14 = document.getElementById('btnfechar14')
+
+    janelaModal14.classList.remove("escondejanelamodal1");
+    janelaModal14.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+
+    btnfechar14.onclick=function(){
+        janelaModal14.classList.add("escondejanelamodal1");
+        janelaModal14.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+   
+}
+function clickModal15(img){
+    const janelaModal15 = document.getElementById('janelamodal15')
+    const txtmodal = document.getElementById('txtmodal')
+    const btnfechar15 = document.getElementById('btnfechar15')
+
+    janelaModal15.classList.remove("escondejanelamodal1");
+    janelaModal15.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+   
+    btnfechar15.onclick=function(){
+        janelaModal15.classList.add("escondejanelamodal1");
+        janelaModal15.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+    
+   
+}
+function clickModal16(img){
+    const janelaModal16 = document.getElementById('janelamodal16')
+    const txtmodal = document.getElementById('txtmodal')
+    const btnfechar16 = document.getElementById('btnfechar16')
+
+    janelaModal16.classList.remove("escondejanelamodal1");
+    janelaModal16.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+
+    btnfechar16.onclick=function(){
+        janelaModal16.classList.add("escondejanelamodal1");
+        janelaModal16.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+    btnfechar30.onclick=function(){
+        janelaModal16.classList.add("escondejanelamodal1");
+        janelaModal16.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+   
+}
+function clickModal17(img){
+    const janelaModal17 = document.getElementById('janelamodal17')
+    const txtmodal = document.getElementById('txtmodal')
+    const btnfechar17 = document.getElementById('btnfechar17')
+
+    janelaModal17.classList.remove("escondejanelamodal1");
+    janelaModal17.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+   
+    btnfechar17.onclick=function(){
+        janelaModal17.classList.add("escondejanelamodal1");
+        janelaModal17.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+   
+}
+function clickModal18(img){
+    const janelaModal18 = document.getElementById('janelamodal18')
+    const txtmodal = document.getElementById('txtmodal')
+    const btnfechar18 = document.getElementById('btnfechar18')
+
+    janelaModal18.classList.remove("escondejanelamodal1");
+    janelaModal18.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+
+    btnfechar18.onclick=function(){
+        janelaModal18.classList.add("escondejanelamodal1");
+        janelaModal18.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+    btnfechar50.onclick=function(){
+        janelaModal18.classList.add("escondejanelamodal1");
+        janelaModal18.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+   
+}
+function clickModal19(img){
+    const janelaModal19 = document.getElementById('janelamodal19')
+    const txtmodal = document.getElementById('txtmodal')
+    const btnfechar19 = document.getElementById('btnfechar19')
+
+    janelaModal19.classList.remove("escondejanelamodal1");
+    janelaModal19.classList.add("mostrajanelamodal1");
+    document.body.style.overflow='hidden';
+   
+    btnfechar19.onclick=function(){
+        janelaModal19.classList.add("escondejanelamodal1");
+        janelaModal19.classList.remove("mostrajanelamodal1");
+        document.body.style.overflow='auto';
+    }
+   
+}
